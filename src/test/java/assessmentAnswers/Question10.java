9f2eed0b21368351c99b4f99dadd5d3713f49b63
Idoreyin Ankoh <iankoh@softalliance.co.ng>
2023-04-13 02:17:35 +0100

    assignment answers

--- src/test/java/assessmentAnswers/Question10.java
+++ src/test/java/assessmentAnswers/Question10.java
@@ -1,4 +1,31 @@
 package assessmentAnswers;
 
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 public class Question10 {
+    public String textToSentence(String str ){
+
+        String s = new String(String.valueOf(str.charAt(0)));
+        s.toUpperCase();
+        for (int i = 0; i < str.length(); i++) {
+            char chars= str.charAt(i);
+            if (chars == '.') {
+                while(str.charAt(i) != ' '){
+                    s=String.valueOf(chars);
+                    s.toUpperCase();
+                }
+            }
+        }
+        return str;
+    }
+    @Test
+    public void testingSentence() {
+        System.out.println( textToSentence("me and my guys. and after that"));
+        String expected = "Me and my guys. And after that";
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(expected, textToSentence("me and my guys. and after that"));
+        softAssert.assertAll();
+
+    }
 }
--- src/test/java/assessmentAnswers/Question9.java
+++ src/test/java/assessmentAnswers/Question9.java
@@ -1,4 +1,24 @@
 package assessmentAnswers;
 
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 public class Question9 {
+    public String textToUpperCase(String str ){
+        if(str == null){
+            System.out.println("Please pass a valid string");
+        }else {
+            return str.toUpperCase();
+        }
+        return str;
+    }
+    @Test
+    public void testing() {
+        String expected = "ANKOH WAMBISAKA LAMANGO";
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(expected, textToUpperCase("Ankoh Wambisaka Lamango"));
+        softAssert.assertAll();
+
+    }
+
 }
